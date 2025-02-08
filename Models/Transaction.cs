988eebc66dce477c34cc3c3153878630e76e91ce
Dylan <109418123+WebbD777@users.noreply.github.com>
2025-02-08 21:23:04 +0200

    Going to add DB

---
+++ Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace My_Website.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int transactionID { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime date { get; set; }
+        public string transactionType { get; set; }
+        public decimal amount { get; set; }
+        public int userID { get; set; }
+        public User User { get; set; } // Foreign key
+        public string assetName { get; set; }
+        public string assetType { get; set; }
+        public string? symbol { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace My_Website.Models
+{
+    public class User
+    {
+        [Key]
+        public string userID { get; set; }
+        public string password { get; set; }
+
+    }
+}
